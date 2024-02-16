00fdaa8c7f4e041f1575101ed31b27844070490d
nicod12 <dumenicolas45@gmail.com>
2024-02-16 17:40:13 -0300

    upd

--- src/app/donate/page.tsx
+++ src/app/donate/page.tsx
@@ -8,7 +8,7 @@ import { IoCardSharp } from "react-icons/io5";
 
 export default function DonatePage() {
     return(
-        <section className="flex flex-col items-center mt-10">
+        <section className="flex flex-col items-center mt-14">
             <h2 className="text-orange-400 text-[22px] lg:text-[25px] xl:text-[26px] 2xl:text-[30px]">Donations</h2>
             <p className="py-4 lg:py-8 px-8 text-center text-gray-300 text-[16px] lg:text-[18px] xl:text-[20px] 2xl:text-[22px]">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, incidunt inventore ullam voluptatibus laboriosam illum voluptates similique. Commodi repellat quasi error atque nobis, similique obcaecati cum temporibus, minima pariatur aspernatur?
             Aliquid nisi minus, repellendus, mollitia doloribus ad modi est eveniet minima atque possimus impedit explicabo! Est assumenda quae rem eveniet dolorem quod reiciendis eius harum alias quam deleniti, atque officia.
@@ -21,7 +21,7 @@ export default function DonatePage() {
                     <Link href="https://www.mercadopago.com.ar/" className="text-[45px] lg:text-[58px] xl:text-[58px] 2xl:text-[60px]">
                         <div className="flex flex-col items-center">
                             <SiMercadopago />
-                            <span className="text-[12px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">Mercado Pago</span>
+                            <span className="text-[10px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">Mercado Pago</span>
                         </div>
                     </Link>
                 </li>
@@ -29,7 +29,7 @@ export default function DonatePage() {
                     <Link href="https://www.paypal.com/" className="text-[45px] lg:text-[58px] xl:text-[58px] 2xl:text-[60px]">
                         <div className="flex flex-col items-center">
                             <FaCcPaypal />
-                            <span className="text-[12px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">PayPal</span>
+                            <span className="text-[10px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">PayPal</span>
                         </div>
                     </Link>
                 </li>
@@ -37,7 +37,7 @@ export default function DonatePage() {
                     <Link href="https://www.example.com/" className="text-[45px] lg:text-[58px] xl:text-[58px] 2xl:text-[60px]">
                         <div className="flex flex-col items-center">
                             <IoCardSharp />
-                            <span className="text-[12px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">Credit Card</span>
+                            <span className="text-[10px] lg:text-[16px] xl:text-[18px] 2xl:text-[20px]">Credit Card</span>
                         </div>
                     </Link>
                 </li>
