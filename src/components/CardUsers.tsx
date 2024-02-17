7489690311647b4ded048e8c60c92e19efa79030
nicod12 <dumenicolas45@gmail.com>
2024-02-16 22:41:27 -0300

    udp

--- src/components/CardUsers.tsx
+++ src/components/CardUsers.tsx
@@ -14,16 +14,16 @@ async function loadUsers() {
         <section className=" grid gap-3 lg:gap-0 lg:grid-cols-3 lg:p-6 xl:gap-0 xl:grid-cols-3 xl:p-6 2xl:gap-0 2xl:grid-cols-3 2xl:p-6">
         {users.data.map((user: any) => (
           <div key={user.id} className="grid grid-cols-1 border border-gray-200 lg:border-0">
-            <article className="relative">
+            <article className="relative z-10">
               <Image 
-                className="relative"
+                className="relative z-10"
                 width={180}
                 height={200}
                 src={user.avatar} 
                 alt={`Avatar of ${user.first_name}`} 
                 loading="eager"
               />
-              <h3 className="absolute -bottom-4 left-2 transform -translate-y-1/2 text-black font-bold text-[22px]">
+              <h3 className="absolute z-10 -bottom-4 left-2 transform -translate-y-1/2 text-black font-bold text-[22px]">
                 {`${user.first_name} ${user.last_name}`}
               </h3>
             </article>
